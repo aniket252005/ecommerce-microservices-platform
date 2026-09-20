2fb6bc9ef6a92b081df2c97e14a9efbeef6ab853
Aniket Dileep Pandey <149816342+aniket252005@users.noreply.github.com>
2026-09-20 15:44:37 +0530

    Initial commit: E-commerce microservices platform

---
+++ database/users-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (created_at, email, name, password, updated_at, username)
+VALUES ('2023-08-01 08:00:00', 'aniket@example.com', 'Aniket',
+        'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', '2023-08-01 08:00:00', 'aniket'),
+       ('2023-08-01 09:00:00', 'aniket2@example.com', 'Aniket User',
+        'c775e7b757ede630cd0aa1113bd102661ab38829ca52a6422ab782862f268646', '2023-08-01 09:00:00', 'aniket2');
+
